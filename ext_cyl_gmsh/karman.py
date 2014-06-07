f2d572a7a5d013f122df7369b6401bcf650b4b1f
cpraveen <cpraveen@gmail.com>
2014-06-07 14:15:13 +0200

    Python script to convert mesh

---
+++ ext_cyl_gmsh/karman.py
@@ -0,0 +1,9 @@
+from mshconvert import *
+
+radius = 0.05
+
+cyl={'type':'C', 'radius':radius, 'depth':3, 'circle_center':(0,0)}
+
+convert('karman.msh', \
+        curves={2:cyl}, \
+        bcs={1:'v', 2:'W', 3:'O', 4:'O', 5:'O'})
